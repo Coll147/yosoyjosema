962dfa97fef4086cb10b0d39b1473307bd93acd1
aaroncoll147@gmail.com <aaroncoll147@gmail.com>
2023-09-06 20:42:54 +0200

    Menu y imagen

--- js/image_anim.js
+++ js/image_anim.js
@@ -1,10 +1,14 @@
 const imagen = document.getElementById('cambiarImagen');
-const imagenes = ['imagen1.jpg', 'imagen2.jpg', 'imagen3.jpg'];
+const imagenes = ['/assets/josemaRosa.png', '/assets/josemaCalvo.png'];
 let index = 0;
 
 function cambiarImagen() {
     index = (index + 1) % imagenes.length;
-    imagen.src = imagenes[index];
+    imagen.style.opacity = '0'; // Establecemos la opacidad a cero para ocultar la imagen
+    setTimeout(() => {
+        imagen.src = imagenes[index];
+        imagen.style.opacity = '1'; // Restauramos la opacidad para mostrar la nueva imagen
+    }, 500); // Establecemos un retardo de 500ms antes de mostrar la nueva imagen
 }
 
-setInterval(cambiarImagen, 3000); // Cambiar la imagen cada 3 segundos
+setInterval(cambiarImagen, 6000); // Cambiar la imagen cada 6 segundos (ajustado)
